c52a91488658f9ae2978ce7cbee997c3a614aa65
prandrea <prandrea@utexas.edu>
2022-05-17 23:54:50 -0500

    combined selected arrays and created randomizer

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 //write the generate password function that is called in "writePassword" function
 function generatePassword() {
-  
- // var passwordcreated = 
-  return "Andrea"
-}
 
 //Create arrays for password criteria
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
@@ -16,11 +12,10 @@ var passwordcreated = []
 var passwordLength = parseInt(window.prompt ("Your password must be between 8 and 128 characters in length. Please enter password length:"));
 
 //Ensure length is between 8-128
-//if  password length 8=<  <=128 then move on to password criteria questions else alert please choose number between 8 and 128
+    //if  password length 8=<  <=128 then move on to password criteria questions else alert please choose number between 8 and 128
 while (passwordLength < 8 || passwordLength > 128) {
   var passwordLength = parseInt(window.prompt ("Your password must be between 8 and 128 characters in length. Please enter password length:"));
 }
-
 console.log(passwordLength)
 
   // Ask user for password criteria
@@ -29,33 +24,44 @@ console.log(passwordLength)
   var includeNumber = window.confirm("Would you like to include numbers?");
   var includeSpecial = window.confirm("Would you like to include special characters?");
 
-
-
-
-
-
-
 //Ensure at least one character type is selected
+while (
+   !includeLower &&
+   !includeUpper &&
+   !includeNumber &&
+   !includeSpecial
+ ) 
+ { window.alert("Choose at least one of the character types in order to generate your password.");
+    var includeLower = window.confirm("Would you like to include lowercase letters?");
+    var includeUpper = window.confirm("Would you like to include uppercase letters?");
+    var includeNumber = window.confirm("Would you like to include numbers?");
+    var includeSpecial = window.confirm("Would you like to include special characters?");
+   }
 
-// while (
-//   !passwordInfo.lowercase &&
-//   !passwordInfo.uppercase &&
-//   !passwordInfo.numeric &&
-//   !passwordInfo.special
-// ) { alert("Choose at least one of the conditions in order to generate your password.");
-// return;
-// }
+//Combine arrays based on selected criteria
+  //How do I take from one array and push to another array? How do I fill in the remaining spaces of the array?
+if (includeNumber) {
+  passwordcreated = numbers.concat(passwordcreated)
+}
+if (includeSpecial) {
+  passwordcreated = special.concat(passwordcreated)
+}
+if (includeLower) {
+  passwordcreated = lowerCase.concat(passwordcreated)
+}
+if (includeUpper) {
+  passwordcreated = upperCase.concat(passwordcreated)
+}
 
 
-//generate random 
-var random = Math.floor(Math.random())
+//use passwordcreated array to generate random password
+for (var i = 0; i < passwordLength; i++){
+var createRandom = Math.floor(Math.random() * passwordcreated.length);
+password = password + passwordcreated[createRandom];
+}
+return password;
+}
 
-
-
-
-//How do I take from one array and push to another array? How do I fill in the remaining spaces of the array?
-
-//if includeLower== true then push to password array
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
